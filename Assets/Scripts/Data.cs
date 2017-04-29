bc026424202c2a30e61e9bc8a319dd8291dea69d
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-04-29 13:28:23 +0700

    added health damager & health state reporter

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -2,9 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum HealthLevel
+{
+    SlightlyDamaged,
+    MediumDamaged,
+    HighlyDamaged
+}
+
 public static class Data
 {
-    public static float FloorHeight { get; set; }
+    static Data()
+    {
+        PlayerHealth = 100f;
+    }
 
     public static MainController Controller { get; set; }
+
+    public static float PlayerHealth { get; set; }
+    public static float FloorHeight { get; set; }
+    public static float IsSmokeHurts { get; set; }    
+    
+    public static void DamageUser(float damageValue)
+    {
+        PlayerHealth -= damageValue;
+        if (PlayerHealth < 0)
+        {
+            Controller.KillPlayer();
+        }
+        else
+        {
+            if (PlayerHealth > 80 && PlayerHealth < 100)
+            {
+                Controller.NotifyAboutHealth(HealthLevel.SlightlyDamaged);
+            }
+            else if (PlayerHealth > 40 && PlayerHealth <= 80)
+            {
+                Controller.NotifyAboutHealth(HealthLevel.MediumDamaged);
+            }
+            else
+            {
+                Controller.NotifyAboutHealth(HealthLevel.HighlyDamaged);
+            }
+        }
+    }
 }
