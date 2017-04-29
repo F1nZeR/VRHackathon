a47e3471f9914adb43275f597b76a911243cbec9
Dmitry <ChebanovDD@gmail.com>
2017-04-29 10:56:30 +0300

    Added duststorm.

---
+++ Assets/Scripts/Duststorm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duststorm : MonoBehaviour 
+{
+
+	public Transform MainCamera;
+
+	public float Height = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = new Vector3 (MainCamera.position.x, Height, MainCamera.position.z);
+	}
+}
