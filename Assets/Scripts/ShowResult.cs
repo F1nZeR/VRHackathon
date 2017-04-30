d985526c58e8192d605165626ee87224ce44f97b
Dmitry <ChebanovDD@gmail.com>
2017-04-30 10:03:48 +0700

    Added result message.

---
+++ Assets/Scripts/ShowResult.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowResult : MonoBehaviour {
+
+    public Text Result;
+
+    public void Start()
+    {
+        Show();
+    }
+
+    public void Show()
+    {
+        var header = Data.IsSurvive ? "Вы выжили" : "Вы умерли";
+        var baby = Data.IsBabyTaken ? "Вы спасли ребенка." : "Вы НЕ спасли ребенка";
+        var alarm = Data.IsAlarmPressed ? "Вы включили тревогу" : "Вы НЕ включили тревогу";
+
+        Result.text = header + "\n\n" + baby + "\n" + alarm;
+    }
+}
